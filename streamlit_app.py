09234553079087679a288eac8639372b9bcd724d
di-b-hoole <123636281+di-b-hoole@users.noreply.github.com>
2023-01-26 14:51:01 +0200

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     SQL = """
     SELECT *
-    FROM "https://docs.google.com/spreadsheets/d/1l8TDEJLFmI-pYekile_iCGWizUa2fReeFlkpKJ9Zgw4/edit#gid=0"
-    """#.format(sheet_url = sheet_url)
+    FROM "https://docs.google.com/spreadsheets/d/1_rN3lm0R_bU3NemO0s9pbFkY5LQPcuy1pscv8ZXPtg8/edit#gid=1648320094"
+    """
     
     for row in cursor.execute(SQL):
         print(row)
