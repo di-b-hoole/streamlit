1a2e243f8cc7ab915f9aca9a2ac2e037ed95929b
di-b-hoole <123636281+di-b-hoole@users.noreply.github.com>
2023-01-26 14:55:47 +0200

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,20 @@ import streamlit as st
 #from gsheetsdb import connect
 from shillelagh.backends.apsw.db import connect
 
+conn = connect(":memory:")
+cursor = conn.cursor()
 
-"""
-A simple example showing the GSheets adapter.
-"""
-from shillelagh.backends.apsw.db import connect
+@st.cache(ttl=600)
+def run_query(query):
+    rows = cursor.execute(query)
+    rows = rows.fetchall()
+    return rows
 
 sheet_url = st.secrets["public_gsheets_url"]
-st.write(sheet_url)
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-if __name__ == "__main__":
-    connection = connect(":memory:")
-    cursor = connection.cursor()
-
-    SQL = """
-    SELECT *
-    FROM "https://docs.google.com/spreadsheets/d/1_rN3lm0R_bU3NemO0s9pbFkY5LQPcuy1pscv8ZXPtg8/edit#gid=1648320094"
-    """
-    
-    for row in cursor.execute(SQL):
-        print(row)
-
+for row in cursor.execute(query):
+    st.write(row)
 #sheet_url = st.secrets["public_gsheets_url"]
 #
 ## Create a connection object.
