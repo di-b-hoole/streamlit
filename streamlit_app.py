6ccc5a22d65daedd41e97deca208c79651113c0e
di-b-hoole <123636281+di-b-hoole@users.noreply.github.com>
2023-01-26 15:07:36 +0200

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ from shillelagh.backends.apsw.db import connect
 conn = connect(":memory:")
 cursor = conn.cursor()
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def run_query(query):
     rows = cursor.execute(query)
@@ -16,36 +18,14 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 query = f'SELECT * FROM "{sheet_url}"'
 
+# Print results.
 for row in cursor.execute(query):
-    st.write(row)
-#sheet_url = st.secrets["public_gsheets_url"]
-#
-## Create a connection object.
-#connection = connect(f'{sheet_url}')
-#cursor = connection.cursor()
-#
-#
-#query = f'SELECT * FROM "{sheet_url}"'
-#for row in cursor.execute(query):
-#    print(row)
+    st.write(f"{row.Client} has a :{row.Value}:")
 
+query = (
+    f'UPDATE "{sheet_url}"'
+    "SET Client = 'Brydon'"
+    "WHERE Client = 'Bravo'"
+)
 
-
-
-
-#conn = connect()
-#
-## Perform SQL query on the Google Sheet.
-## Uses st.cache to only rerun when the query changes or after 10 min.
-#@st.cache(ttl=600)
-#def run_query(query):
-#    rows = conn.execute(query, headers=1)
-#    rows = rows.fetchall()
-#    return rows
-#
-#sheet_url = st.secrets["public_gsheets_url"]
-#rows = run_query(f'SELECT * FROM "{sheet_url}"')
-#
-## Print results.
-#for row in rows:
-#    st.write(f"{row.Client} has a :{row.Value}:")
+st.write(query)
